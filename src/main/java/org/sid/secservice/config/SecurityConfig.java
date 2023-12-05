751b64fd440a317fdadd7df99040bf7af42a754c
adnane zaki <adnanezaki132@gmail.com>
2023-12-05 17:09:49 +0100

    spring security

--- src/main/java/org/sid/secservice/config/SecurityConfig.java
+++ src/main/java/org/sid/secservice/config/SecurityConfig.java
@@ -1,25 +1,19 @@
 package org.sid.secservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests((authz) -> authz
+                        .anyRequest().authenticated())
+                .httpBasic();
+        return http.build();
     }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-        http.authorizeRequests().anyRequest().permitAll();
-    }
-
 }
